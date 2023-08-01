f5e416ef085b861a1f848b0c32425b1ef1342a30
KidP2H <thjnhsoajca@gmail.com>
2023-08-01 22:53:16 +0700

    feat(setup): jwt

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,16 @@ import { UserController } from "./user.controller";
 import { UserCryptService } from "./user.crypt";
 import { NearModule } from "src/near/near.module";
 import { UserContract } from "./user.contract";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [NearModule],
+  imports: [
+    NearModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY,
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService, UserCryptService, UserContract],
   exports: [UserService],
