570ba0ee9326f40664e15c30219997f2f1059da9
KidP2H <thjnhsoajca@gmail.com>
2023-08-01 23:05:25 +0700

    refactor: format code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(+(<string>process.env.PORT) || 3000);
 }
 bootstrap();
