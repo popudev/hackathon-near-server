d2b881ffb2711aa7a3bde1697c42ea58124be1d4
KidP2H <thjnhsoajca@gmail.com>
2023-08-01 23:17:31 +0700

    feat: add api login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,29 +1,54 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { Response } from "express";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { LoginUserDto } from "./dto/login-user.dto";
+import { JwtService } from "@nestjs/jwt";
 
 @Controller("user")
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService, private jwtService: JwtService) {}
+
+  @Post("login")
+  async login(@Body() LoginUserDto: LoginUserDto, @Res({ passthrough: true }) res: Response) {
+    const { username, password } = LoginUserDto;
+    const result = await this.userService.login({ username, password });
+    if (result) {
+      const accessToken = await this.jwtService.signAsync(result);
+      res.status(HttpStatus.OK).send({ accessToken, status: false });
+      return;
+    }
+    res.status(HttpStatus.UNAUTHORIZED).send({ accessToken: null, status: true });
+  }
+
+  @Get("createAdmin")
+  createAdmin() {
+    return this.userService.createAdmin();
+  }
 
   @Get("login")
-  login(@Body() LoginUserDto: LoginUserDto) {
-    const user = { username: "thanh", password: "123" };
-    return this.userService.login(user);
+  test(@Body() LoginUserDto: LoginUserDto) {
+    console.log("test");
   }
 
   @Get("mock")
-  mock(){
+  mock() {
     const users = [
-      {  full_name:"Bui Manh Thanh", date_of_birth:"25/10/2002",email:"manhthanh147@gmail.com",phone:"0328735659",national_identity_card:"159753",national_identity_card_date:"25/10/2023" },
-    ]
-    return users.forEach(u=>this.userService.create(u));
+      {
+        full_name: "Bui Manh Thanh",
+        date_of_birth: "25/10/2002",
+        email: "manhthanh147@gmail.com",
+        phone: "0328735659",
+        national_identity_card: "159753",
+        national_identity_card_date: "25/10/2023",
+      },
+    ];
+    return users.forEach((u) => this.userService.create(u));
   }
 
   @Get()
-  findAll(){
+  findAll() {
     return this.userService.findAll();
   }
 
