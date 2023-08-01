0b43a9be84b99b8d4f84690511409793ac94aa67
KidP2H <thjnhsoajca@gmail.com>
2023-08-01 23:06:31 +0700

    refactor: change jwt service to global

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,18 @@ import { NearModule } from "./near/near.module";
 import { ConfigModule } from "@nestjs/config";
 import { UserModule } from "./user/user.module";
 import { RoleModule } from "./role/role.module";
-import { MajorModule } from './major/major.module';
+import { MajorModule } from "./major/major.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
   imports: [
     NearModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY,
     }),
     UserModule,
     RoleModule,
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,16 +4,9 @@ import { UserController } from "./user.controller";
 import { UserCryptService } from "./user.crypt";
 import { NearModule } from "src/near/near.module";
 import { UserContract } from "./user.contract";
-import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [
-    NearModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.SECRET_KEY,
-    }),
-  ],
+  imports: [NearModule],
   controllers: [UserController],
   providers: [UserService, UserCryptService, UserContract],
   exports: [UserService],
