5387441260651e57372783f3c1efa23c8b1cd040
KidP2H <thjnhsoajca@gmail.com>
2023-08-03 02:44:50 +0700

    feat: add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nodeExternals = require("webpack-node-externals");
+const { RunScriptWebpackPlugin } = require("run-script-webpack-plugin");
+const path = require("path");
+
+module.exports = function (options, webpack) {
+  return {
+    ...options,
+    entry: ["webpack/hot/poll?100", options.entry],
+    externals: [
+      nodeExternals({
+        allowlist: ["webpack/hot/poll?100"],
+      }),
+    ],
+    mode: "production",
+    devtool: false,
+    plugins: [
+      ...options.plugins,
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.WatchIgnorePlugin({
+        paths: [/\.js$/, /\.d\.ts$/],
+      }),
+      new webpack.SourceMapDevToolPlugin({}),
+      new RunScriptWebpackPlugin({ name: options.output.filename }),
+    ],
+  };
+};
