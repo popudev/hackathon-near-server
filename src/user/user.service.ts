98d84ce706fdc646716ce31baeb8ba1b6876d257
Bùi Mạnh Thành TTS <manhthanh147@gmail.com>
2023-08-03 20:08:12 +0700

    feat:assigment

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -48,7 +48,7 @@ export class UserService {
 
   async findAllInstructor() {
     const users = await this.findAll();
-    return users.map((u) => u.role === "Instructor");
+    return users.filter((u) => u.role === "Instructor");
   }
 
   async activeStudent(activeStudentDto: ActiveUserDto) {
